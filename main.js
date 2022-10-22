b7a91a2eb5d262fe9c4fc82b23c6de49ae059aee
masan894 <MasanaMail762@gmail.com>
2022-10-22 18:03:02 +0900

    ポートフォリオサイトの開発を進めました

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-//window.onload = function () {
-const title1 = document.getElementById("title1");
+window.onload = (event) => {
+  const myH1 = document.getElementById("myh1");
+  let upper = myH1.textContent.toUpperCase();
+  let max = upper.length + 30;
+  for (let i = 1; i < upper.length; i++) {
+    myH1.textContent =
+      '<span style="font-size:' +
+      max-- +
+      'pt; padding-top: 70px;">' +
+      upper.charAt(i) +
+      "</span>";
+  }
+};
